b60e48612591728b87e347ddb8efe6528064aa09
Patrick Boutin <poboutin@gmail.com>
2018-10-08 00:53:46 -0700

    Rollback to stable version.
    This is a working version where mode changes 'on click' of H4 and different character is output based on the current mode.

--- js/script-1.11--character-to-integer.js
+++ js/script-1.11--character-to-integer.js
@@ -8,34 +8,39 @@
 
 let inputArray  = ["18", "12312", "171", "98423", "1208", "216", "11", "500", "18", "241", "0", "32", "20620", "27", "10"],
     outputArray = [],
-    modeArray   = ["U", "L", "P"];
+    modeArray   = ["U", "L", "P"],
+    modeIndex = 2;
 
-function swapMode(modeIndex) {
-  let mode;
+// set modeValue based on modeIndex
+function swapMode() {
+  let modeValue;
   switch (modeIndex) {
     case 0:
       modeIndex++;
-      mode = modeArray[modeIndex];
+      modeValue = modeArray[modeIndex];
       break;
     case 1:
       modeIndex++;
-      mode = modeArray[modeIndex];
+      modeValue = modeArray[modeIndex];
       break;
     case 2:
       modeIndex = 0;
-      mode = modeArray[modeIndex];
+      modeValue = modeArray[modeIndex];
       break;
   }
-  return mode;
+  //console.log('mode is now: ' + modeValue);
+  return modeValue;
 }
 
-function convertInputToCode(input, mode) {
+function convertInputToCode(input, modeValue) {
   let letters     = "abcdefghijklmnopqrstuvwxyz",
-      punctuation = "!?,. ;\"'";
+      punctuation = "!?,. ;\"'",
+      characters  = [],
+      character,
+      divisor;
 
-  // set divisor based on mode
-  let divisor;
-  switch(mode) {
+  // set divisor based on modeValue
+  switch(modeValue) {
     case "U":
     case "L":
       divisor = 27;
@@ -45,39 +50,24 @@ function convertInputToCode(input, mode) {
       break;
   }
 
-  // set possible characters based on mode
-  let possibleCharacters = [];
-  if (mode === "U") {
-    for (let i = 0; letters.length; i++) {
-      possibleCharacters.push(letters[i].toUpperCase());
-    }
-  } else if (mode === "L") {
-    for (let i = 0; letters.length; i++) {
-      possibleCharacters.push(letters[i].toLowerCase());
-    }
-  } else if (mode === "P") {
-    for (let j = 0; punctuation.length; j++) {
-      possibleCharacters.push(punctuation[j]);
-    }
-  }
-
   let characterIndex  = (input % divisor) - 1;
 
-  return console.log(possibleCharacters);
-}
-convertInputToCode(763, "P");
-
-
-function processInput() {
-  for (let i = 0; i < inputArray.length; i++) {
-    console.log('number: ' + inputArray[i]);
+  //set possible characters based on modeValue
+  if (modeValue === "U") {
+    character = letters[characterIndex].toUpperCase();
+  } else if (modeValue === "L") {
+    character = letters[characterIndex].toLowerCase();
+  } else if (modeValue === "P") {
+    character = punctuation[characterIndex];
   }
-}
-//processInput();
 
-let headerEl  = document.getElementById('header-h4');
+  return console.log(`Current mode is ${modeValue} and based on this mode, character is: ${character}.`);
+}
+//convertInputToCode(763, "P");
+
+let headerEl     = document.getElementById('header-h4');
 console.log('starting mode: ' + modeArray[0]);
 headerEl.addEventListener('click', function (e) {
-  let mode = swapMode();
-  convertInputToCode(763, mode);
+  let swap = swapMode();
+  convertInputToCode(763, swap);
 });
