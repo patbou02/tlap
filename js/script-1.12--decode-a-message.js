ed0a15a4acd936716cf2003e9eb1a50fb5f135dd
Patrick Boutin <poboutin@gmail.com>
2018-10-11 01:12:01 -0700

    Update js/script-1.12--decode-a-message.js
    properly swapping out starting number for code based on proper dividor and mode swapping when remainder equals 0.

--- js/script-1.12--decode-a-message.js
+++ js/script-1.12--decode-a-message.js
@@ -7,49 +7,41 @@ console.log("=== 1.12 - Decode a Message ===");
 console.info("page 41 - Decode a Message");
 
 // Global Variables
-let inputArray       = ["18", "12312", "171"],
+let inputArray       = ["18", "12312", "171", "763", "98423", "1208", "216", "11", "500", "18", "241", "0", "32", "20620", "27", "10"],
     modeArray        = ["U", "L", "P"],
     modeIndex        = 0,
-    mode             = modeArray[modeIndex],
+    currentMode      = modeArray[modeIndex],
     letters          = "abcdefghijklmnopqrstuvwxyz".split(''),
     punctuation      = "!?,. ;\"'".split(''),
     uppercaseArray   = [],
     lowercaseArray   = [],
-    punctuationArray = [];
+    punctuationArray = [],
+    divider,
+    characterCode,
+    inputCode,
+    inputCodeArray   = [];
 
-function processInput(input) {
-  for (let i = 0; i < input.length; i++) {
-    console.log(input[i]);
+function convertArrayStringsToNumbers() {
+  for (let i = 0; i < inputArray.length; i++) {
+    inputArray[i] = parseInt(inputArray[i]);
   }
-}
-
-function swapMode() {
-  let modeInEffect;
-  if (modeIndex != "2") {
-    modeIndex++;
-  } else {
-    modeIndex = 0;
-  }
-  modeInEffect = modeArray[modeIndex];
-
-  return modeInEffect;
 }
 
 function initializeArrays(inputArray, mode) {
   if (mode === "U") {
-    console.log('UPPERCASE array initialized');
+    //console.log('UPPERCASE array initialized');
     for (let i = 0; i < inputArray.length; i++) {
       uppercaseArray.push(inputArray[i].toUpperCase());
     }
     return uppercaseArray;
   } else if (mode === "L") {
-    console.log('LOWERCASE array initialized');
+    //console.log('LOWERCASE array initialized');
     for (let i = 0; i < inputArray.length; i++) {
       lowercaseArray.push(inputArray[i].toLowerCase());
     }
     return lowercaseArray;
   } else if (mode === "P") {
-    console.log('PUNCTUATION array initialized');
+    //console.log('PUNCTUATION array initialized');
     for (let i = 0; i < inputArray.length; i++) {
       punctuationArray = inputArray;
     }
@@ -57,13 +49,63 @@ function initializeArrays(inputArray, mode) {
   }
 }
 
+function swapMode() {
+  if (modeIndex != "2") {
+    modeIndex++;
+  } else {
+    modeIndex = 0;
+  }
+  currentMode = modeArray[modeIndex];
+  return currentMode;
+}
+
+function getDivider(mode) {
+  switch(mode) {
+    case "U":
+    case "L":
+      divider = 27;
+      break;
+    case "P":
+      divider = 9;
+      break;
+    default:
+      divider = 0;
+      console.log('Please enter a correct mode in order to get appropriate divider');
+      break;
+  }
+  return divider;
+}
+
+function getCharacterCode(input, divider) {
+  characterCode = input % divider;
+
+  return characterCode;
+}
+
+function processInput(input) {
+  console.log(input);
+  for (let i = 0; i < input.length; i++) {
+    console.log('current mode: ' + currentMode);
+    console.log('starting input: ' + input[i]);
+    inputCode = getCharacterCode(input[i], getDivider(currentMode));
+    console.log('divider: ' + divider);
+    console.log('input code: ' + inputCode);
+    inputCodeArray.push(inputCode);
+    if (inputCodeArray[i] === 0) {
+      currentMode = swapMode();
+      console.log('swapping mode to: ' + currentMode);
+    }
+    console.log('resulting mode: ' + currentMode);
+    console.log('input code in Code array: ' + inputCodeArray[i]);
+    console.log('=== moving on to next input value ===');
+  }
+  console.log(inputCodeArray);
+}
+
+convertArrayStringsToNumbers();
 
 initializeArrays(letters, "U");
 initializeArrays(letters, "L");
 initializeArrays(punctuation, "P");
 
-
-
-//processInput(inputArray);
-//initializeAnswerArrays();
-
+processInput(inputArray);
