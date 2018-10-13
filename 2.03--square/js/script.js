6642dcf51a600c8b7391effd89e7959096fdb43d
Patrick Boutin <poboutin@gmail.com>
2018-10-13 15:05:32 -0700

    renamed folders from 1 to 2 and references to chap1 inside file comments. also updated some page references whithin scripts.

---
+++ 2.03--square/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Shared functions in ../shared/_shared.js file
+let userInput = prompt("Please enter one character (letter, number or symbol)"),
+    dimension = parseInt(prompt("Please enter a dimension"));
+
+function createSquare() {
+  console.info("=== 2.03 - Square === (single console.log - one for entire shape)");
+  console.info("page 27 - Output Patterns: A Square");
+
+  Shared.validateUserInput(userInput);
+  Shared.validateDimension(dimension);
+
+  let square = "";
+
+  for (let i = 0; i < dimension; i++) {
+    for (let j = 0; j < dimension; j++) {
+      square += userInput;
+    }
+    square += globalVars.lineBreak;
+  }
+  return console.log(square);
+}
+
+createSquare();
