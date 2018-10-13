c85b7694173d19fe1d88e64cb91c909363ac4716
Patrick Boutin <poboutin@gmail.com>
2018-10-13 14:13:22 -0700

    created new 1.14 folder and moved files to new folder for case where multiple digits.

---
+++ 1.14--read-multiple-digits-number/js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+let input                 = "4334569"; //prompt("Please enter a positive or negative number.");
+let inputArray            = input.split(''),
+  digitsArray           = [],
+  unitMultiplier,
+  answerNumber,
+  answerArray           = [],
+  multiplierArray = [],
+  finalAnswer = 0;
+
+function readTwoDigitNumber() {
+  console.log("=== 1.14 - Read Two Digit Number PART 2 ===");
+  console.info("page 44 - Tracking State: Read two digit number. This is an expanded version of 1.13--read-two-digit-number so that it can accommodate larger numbers");
+
+  console.log(`Starting number: ${input}`);
+
+  // push numbers to digitsArray
+  for(let i = 0; i < inputArray.length; i++) {
+    digitsArray.push(parseInt(inputArray[i]));
+  }
+
+  console.log('The digits array contains the following digits: [' + digitsArray + '] and has the following length: ' + digitsArray.length);
+
+  function processMultipliers(input) {
+    for(let i = input.length; i > 0; i--) {
+      if (i === input.length) {
+        unitMultiplier = 1;
+      } else {
+        unitMultiplier *= 10;
+      }
+      multiplierArray.unshift(unitMultiplier);
+    }
+    return console.log(multiplierArray);
+  }
+
+  processMultipliers(inputArray);
+
+  function processInput(input) {
+    for(let i = 0; i < digitsArray.length; i++) {
+      answerNumber = digitsArray[i] * multiplierArray[i];
+      answerArray.push(answerNumber);
+      console.log('answer array contains: [' + answerArray + '].');
+      finalAnswer += answerNumber;
+    }
+    return console.log(finalAnswer);
+  }
+
+  processInput(inputArray);
+}
+
+readTwoDigitNumber();
