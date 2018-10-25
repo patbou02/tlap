f64c5085db222700cdbf8d1d27a1637d8af75fe9
Patrick Boutin <poboutin@gmail.com>
2018-10-25 00:26:13 -0700

    insertion sort script

---
+++ 3-04--array-sort-insertion/js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Shared functions in ../shared/_shared.js file
+
+let array = [5, 10, 4, 36, 90, 1],
+    startIndex = 0,
+    endIndex = array.length - 1,
+    temp;
+
+console.log(array);
+
+function insertionSortArray(array) {
+  console.log("=== 3.04 - Arrays: Sorting Array Elements with Insertion Sort ===");
+  console.log("page 60/61 - Insertion Sort");
+  for (let i = startIndex + 1; i <= endIndex; i++) {
+    for (let j = i; ((j > startIndex) && (array[j - 1])) > array[j]; j--) {
+      temp = array[j - 1];
+      array[j - 1] = array[j];
+      array[j] = temp;
+    }
+  }
+}
+
+insertionSortArray(array);
+
+console.log(array);
