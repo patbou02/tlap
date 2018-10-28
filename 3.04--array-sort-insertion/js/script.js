3c45a52f0fc9e32fb169daf9e9c9007b34b30efa
Patrick Boutin <poboutin@gmail.com>
2018-10-27 18:06:05 -0700

    moved variables inside function so they aren't in the global scope.

--- 3.04--array-sort-insertion/js/script.js
+++ 3.04--array-sort-insertion/js/script.js
@@ -2,16 +2,18 @@
 
 // Shared functions in ../shared/_shared.js file
 
-let array = [5, 10, 4, 36, 90, 1],
-    startIndex = 0,
-    endIndex = array.length - 1,
-    temp;
+let array = [5, 10, 4, 36, 90, 1];
 
 console.log(array);
 
 function insertionSortArray(array) {
   console.log("=== 3.04 - Arrays: Sorting Array Elements with Insertion Sort ===");
   console.log("page 60/61 - Insertion Sort");
+
+  let startIndex = 0,
+      endIndex = array.length - 1,
+      temp;
+
   for (let i = startIndex + 1; i <= endIndex; i++) {
     for (let j = i; ((j > startIndex) && (array[j - 1])) > array[j]; j--) {
       temp = array[j - 1];
