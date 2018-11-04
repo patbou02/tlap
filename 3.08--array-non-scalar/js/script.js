e1a8d4c766b2879399269af86d7f979a25e789e5
Patrick Boutin <poboutin@gmail.com>
2018-11-03 23:31:31 -0700

    created function which pulls in value within array nested inside of another array.

---
+++ 3.08--array-non-scalar/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Shared functions in ../shared/_shared.js file
+
+let studentsArray = [
+  [87, 10001, "Fred"],
+  [27, 10002, "Tom"],
+  [100, 10003, "Alistair"],
+  [78, 10004, "Sasha"],
+  [84, 10005, "Erin"],
+  [98, 10006, "Belinda"],
+  [75, 10007, "Leslie"],
+  [70, 10008, "Candy"],
+  [81, 10009, "Aretha"],
+  [68, 10010, "Veronica"],
+];
+
+console.log(studentsArray);
+
+function getHighestGrade(arr) {
+  console.log("=== 3.08 - Arrays: Non Scalar ===");
+  console.log("page 70 - This function gets the highest grade from an array nested within another array and with non integer values.");
+  let highestGrade = studentsArray[0][0];
+  for (let i = 1; i < studentsArray.length; i++) {
+    if (studentsArray[i][0] > highest) {
+      highest = studentsArray[i][0];
+    }
+  }
+  return console.log(highestGrade);
+}
+
+getHighestGrade(studentsArray);
