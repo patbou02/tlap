e500ba17c0fc776650c6070748e4610085bae13d
Patrick Boutin <poboutin@gmail.com>
2018-11-11 16:11:21 -0800

    update commented out text for better understanding of script.

--- 4.01--variable-length-string-manipulation/js/script.js
+++ 4.01--variable-length-string-manipulation/js/script.js
@@ -13,13 +13,13 @@ function convertStringToArray(str) {
 }
 convertStringToArray(inputString);
 
-// Step 2: return array (arr) element at position (pos)
+// Step 2: return array (arr) element at position (pos).
 function characterAt(arr, pos) {
   return arr[pos];
 }
 characterAt(inputString, 1);
 
-// Step 3: combine both #1 and #2 together
+// Step 3: combine both #1 and #2 together.
 function characterAtFinal(str, pos) {
   let arr = str.split('');
 
@@ -30,7 +30,7 @@ characterAtFinal(inputString, 2);
 function append(arr, char) {
   let oldLength = 0;
 
-  // calculate the character that terminates the array (0/null)
+  // find array index whose value equal null or 0.
   while(arr[oldLength] !== 0) {
     oldLength++;
   }
@@ -38,14 +38,14 @@ function append(arr, char) {
   // create new array
   let newArr = [];
 
-  // copy element from original array to new array
+  // copy original array elements into new array.
   for (let i = 0; i < oldLength; i++) {
     newArr[i] = arr[i];
   }
 
-  newArr[oldLength] = char;  // append character (char) to the end of the new array
-  newArr[oldLength + 1] = 0; // append null value (0) to after appended character
-  arr = newArr; // set original array to new array
+  newArr[oldLength] = char;  // append 'char' to end of new array.
+  newArr[oldLength + 1] = 0; // append null value (0) after appended character in new array.
+  arr = newArr; // set original array equal to new array.
 
   return console.log(arr);
 }
